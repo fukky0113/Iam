bd4913870602c7026e026bd1438941fc845418e3
fukky0113 <fukiyama0113@gmail.com>
2024-12-10 21:20:56 +0900

    add test user password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Hello World", user_id: "@hello_wold")
+    @user = User.new(name: "Hello World", user_id: "@hello_wold", password: "password", password_digest: "password")
   end
 
 
@@ -52,4 +52,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+# パスワード 
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimun length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
