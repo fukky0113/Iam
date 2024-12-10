51416e81e00001e15f6bedd156e9f69152d8037a
fukky0113 <fukiyama0113@gmail.com>
2024-12-10 20:44:22 +0900

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :name,    presence: true, length: { maximum: 11 }
+
+    VALUD_EMAIN_REGEX = /@[a-z]+/
+    validates :user_id, presence: true, length: { maximum: 11 },
+                        format: {with: VALUD_EMAIN_REGEX},
+                        uniqueness: true
 end
