efddeed96ff7537e61b629c578124cfd6a6a36e8
fukky0113 <fukiyama0113@gmail.com>
2024-12-11 09:47:32 +0900

    add password model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-    validates :name,    presence: true, length: { maximum: 11 }
+    validates :name,    presence: true, length: {maximum: 11}
 
     VALUD_EMAIN_REGEX = /@[a-z]+/
-    validates :user_id, presence: true, length: { maximum: 11 },
+    validates :user_id, presence: true, length: {maximum: 11},
                         format: {with: VALUD_EMAIN_REGEX},
                         uniqueness: true
+
+    has_secure_password
+    validates :password, presence: true, length: {minimum: 6}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Hello World", user_id: "@hello_wold", password: "password", password_digest: "password")
+    @user = User.new(name: "Hello World", user_id: "@hello_wold", 
+                      password: "password", password_confirmation: "password")
   end
 
 
