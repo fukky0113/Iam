8fe295985e868757a3361199e4dbdfa53101bd22
fukky0113 <fukiyama0113@gmail.com>
2024-12-14 11:07:59 +0900

    add login proc

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(user_id: params[:session][:user_id])
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
 end
