9541020d5f167b81d78d04fdbc3ca04d40189227
fukky0113 <fukiyama0113@gmail.com>
2024-12-15 20:44:40 +0900

    Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,24 +1,64 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
-
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:hogefuga)
   end
+end
 
-  test "login with invalid information" do
+class InvalidPasswordTest < UsersLogin
+  test "login path" do
     get login_path
     assert_template "sessions/new"
+  end
+
+  test "login with valid userid/invalid password" do
     post login_path, params: {session: {user_id: "", password: ""}}
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template "sessions/new"
     get root_path
   end
-  
-  test "login with valid information" do
-    post login_path, params: {session: {user_id: @user.user_id, password: 'hogefuga' } }
+end
+
+class ValidLogin < UsersLogin
+  def setup
+    super
+    post login_path, params: {session: {user_id: @user.user_id, password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test "valid login" do
+    assert is_logged_in? 
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test "successful logout" do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
+    follow_redirect!
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
