67185129da64368190731577bef0cb33d1b8d3af
fukky0113 <fukiyama0113@gmail.com>
2024-12-16 21:38:12 +0900

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_10_122242) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_15_150316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_10_122242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
