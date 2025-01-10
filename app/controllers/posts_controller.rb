33346968fb0dcb37e9caeb76151d8f96760164de
fukky0113 <fukiyama0113@gmail.com>
2025-01-10 15:11:32 +0900

    add post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,19 @@ class PostsController < ApplicationController
     redirect_to posts_path, status: :see_other
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content)
