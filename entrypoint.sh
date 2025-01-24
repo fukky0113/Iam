7e83464501e2f65c2491659d3bb26aca78da6bfb
fukky0113 <fukiyama0113@gmail.com>
2025-01-24 16:55:39 +0900

    デプロイ準備完了

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@ set -e
 
 rm -f /myapp/tmp/pids/server.pid
 
+# bundle exec rails db:create
+bundle exec rails db:migrate
+# bundle exec rails db:seed
+bundle exec rails assets:precompile
+
+
 exec "$@"
 
