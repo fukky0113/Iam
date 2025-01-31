38a271622924c0f755a48f87e9423dd980b3ca97
fukky0113 <fukiyama0113@gmail.com>
2025-01-31 18:23:30 +0900

    修正 title追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,17 @@ module ApplicationHelper
   def string_slice_0to20(text)
     text.slice(0,20)
   end
+
   def string_slice_0to15(text)
     text.slice(0,15)
+  end
+
+  def full_title(page_title = '')
+    base_title = 'Iam'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
 end
