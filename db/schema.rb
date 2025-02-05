1ce8f9864b00bcf40c78794477ceffbae7b7767f
fukky0113 <fukiyama0113@gmail.com>
2025-02-05 09:21:06 +0900

    追加 Skillモデルと初期シード

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_04_055918) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_04_235444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_04_055918) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "user_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
+    t.string "skill_category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,10 @@ User.create!(user_id: "@onetime",
               password: "onetime",
               password_confirmation: "onetime",
               admin: true)
+
+u_json = ActiveSupport::JSON.decode(File.read(Rails.root.join("db", "skills.json")))
+
+u_json.each do |d|
+  Skill.create!(skill_name: d["skill"],
+                skill_category: d["category"])
+end
